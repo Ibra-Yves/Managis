721c75b4ce0062446aee53aecb0004a6e4553891
Liber Maxime <44001018+LiberTMx@users.noreply.github.com>
2019-10-01 13:25:02 +0200

    Adapt form connexion

--- Web/MANAGIS/INC/connexion.php
+++ Web/MANAGIS/INC/connexion.php
@@ -1,19 +1,29 @@
-<div class="container login-container">
-    <link href="CSS/connexion.css" rel="stylesheet">
-    <div class="row">
-        <div class="col-md-6 login-form-1">
-            <h3>Connexion</h3>
+    <link href="CSS/style.css" rel="stylesheet">
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <div class="row centered-form">
+        <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Connectez-vous</h3>
+                </div>
+                <div class="panel-body">
             <form id="formConnexion" class="formConnexion" name="formConnexion" method="post" action="validation.php">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Pseudo" value="" id="pseudo" name="pseudo"/>
+                        <div class="row">
+                            <div class="col-xs-6 col-sm-6 col-md-6">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Pseudo" value="" id="pseudo" name="pseudo"/>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Mot de passe" value="" id="mdp" name="mdp"/>
+                        </div>
+
+                        <input type="submit" class="btn btn-primary align-middle" name="form" value="connexion" />
+                    </form>
                 </div>
-                <div class="form-group">
-                    <input type="password" class="form-control" placeholder="Mot de passe" value="" id="mdp" name="mdp"/>
-                </div>
-                <div class="form-group">
-                    <input type="submit" class="btnSubmit" name="form" value="connexion" />
-                </div>
-            </form>
+            </div>
         </div>
     </div>
 </div>
