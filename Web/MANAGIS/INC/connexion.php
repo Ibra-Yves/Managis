9e9a6fdebbce481013fd87a76256c4b0ed87d56c
domad007 <44199654+domad007@users.noreply.github.com>
2019-10-01 09:57:10 +0200

    Tout le bordel refactorise
    Reste plus qu'a gerer la connexion et inscirption

--- Web/MANAGIS/INC/connexion.php
+++ Web/MANAGIS/INC/connexion.php
@@ -1,14 +1,22 @@
-<form id="formConnexion" class="formConnexion" name="formConnexion" method="post" action="validation.php">
-    <fieldset id="fieldsetConnexion">
-        <legend>Connectez vous</legend>
-
-        <label for="pseudo">Pseudo</label>
-        <input type="text" id="pseudo" name="pseudo"><br>
-
-
-        <label for="mdp">Mot de passe</label>
-        <input type="password" id="mdp" name="mdp"><br>
-
-        <input type="submit" name="form" value="connexion">
-    </fieldset>
-</form>
+<div class="container login-container">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link href="../CSS/connexion.css" rel="stylesheet">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <div class="row">
+        <div class="col-md-6 login-form-1">
+            <h3>Connexion</h3>
+            <form id="formConnexion" class="formConnexion" name="formConnexion" method="post" action="validation.php">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Pseudo" value="" id="pseudo" name="pseudo"/>
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder="Mot de passe" value="" id="mdp" name="mdp"/>
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btnSubmit" name="form" value="connexion" />
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
--- Web/MANAGIS/INC/template.php
+++ Web/MANAGIS/INC/template.php
@@ -1,18 +1,156 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <script type="text/javascript" src="JS/jquery.js"></script>
-        <script type="text/javascript" src="JS/main.js"></script>
-    </head>
-    <body>
-        <a href="inscription.php">Inscription</a>
-        <a href="connexion.php"> Connexion</a>
-    <div id="formulaire">
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>MANAGIS WEBSITE</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Bootstrap core CSS -->
+    <link href="CSS/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="CSS/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="CSS/style.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="IMG/favicon.png" />
+</head>
+
+<body>
+
+<header>
+    <!--Navbar-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div class="container-fluid">
+            <!-- Navbar brand -->
+            <a class="navbar-brand" href="#">MANAGIS</a>
+
+            <!-- Collapse button -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+                    aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Collapsible content -->
+            <div class="collapse navbar-collapse" id="basicExampleNav">
+
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a href="inscription.php" class="btn btn-outline-dark">Inscription</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="connexion.php" class="btn btn-outline-dark">Connexion</a>
+                    </li>
+                    <li class="nav-item">
+                        <button type="button" class="btn btn-outline-dark">Qui sommes-nous?</button>
+                    </li>
+                </ul>
+                <!-- Links -->
+            </div>
+        </div>
+        <!-- Collapsible content -->
+
+    </nav>
+    <!--/.Navbar-->
+    <div id="intro" class="view">
+        <div class="full-bg-img">
+            <!--<div class="container">
+                <button type="button" class="btn aqua-gradient bouton1">Inscription</button>
+                <button type="button" class="btn aqua-gradient bouton2">Connexion</button>
+            </div>-->
+        </div>
     </div>
-    <div id="error">
-        <p id="wrongUser"></p>
-        <p id="wrongMail"></p>
-        <p id="wrongPass"></p>
     </div>
-    </body>
+
+
+</header>
+
+<main class="container-fluid colormain">
+
+    <div class="row">
+        <div class="col-md-4 marge">
+            <img class="rounded mx-auto d-block marge rounded-circle" src="IMG/managis.png" height="200px" width="200px"align="center">
+
+            <h2 align="center">Qu'est ce que Managis ?</h2>
+            <p align="center">
+                <br>
+                Il s'agit d'une application servant à savoir qui apporte quoi lors de vos événements.<br>
+                Finit de se retrouver en manque d'alcool ou bien de chips en soirée !<br>
+                Prévoyez tout à l'avance et tout ce passera pour le mieux !<br>
+            </p>
+        </div>
+        <div class="col-md-4 marge ">
+            <img class="rounded mx-auto d-block marge rounded-circle" src="IMG/soireAmis.jpg" height="200px" width="200px"align="center">
+            <h2 align="center">Pourquoi l'utiliser ?</h2>
+            <p align="center">
+                <br>
+                Enormément de possibilités !<br>
+                Un voyage entre amis, une soirée, une excursion, une raclette ?<br>
+                Managis sera le partenaire essentiel de la plupart de vos événements !
+            </p>
+        </div>
+        <div class="col-md-4 marge">
+            <img class="rounded mx-auto d-block marge rounded-circle" src="IMG/avantage.jpeg" height="200px" width="200px" align="center">
+
+            <h2 align="center">Le petit plus!</h2>
+            <p align="center">
+                <br>
+                Finis de jeter vos restes ! Mettez les dans un panier pour votre prochain événement !<br>
+                De plus nous offrons un système de covoiturage ce qui évite d'avoir 5 voitures de 1 personnes pour la soirée !<br>
+                Devenez plus éco-responsable en vous amusant !<br>
+            </p>
+        </div>
+    </div>
+</main>
+
+<!-- Footer -->
+<footer class="page-footer font-small grey darken-1 fixed-bottom container-fluid">
+
+    <!-- Footer Elements -->
+    <div class="container">
+        <!-- Grid row-->
+        <div class="row">
+            <!-- Grid column -->
+            <div class="col-md-12 py-1 ">
+                <div class="mb-1 flex-center textfont"><span class ="espace">Retrouvez-nous sur :</span>
+                    <!-- Facebook -->
+                    <a class="fb-ic">
+                        <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+                    </a>
+                    <!-- Twitter -->
+                    <a class="tw-ic">
+                        <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+                    </a>
+                    <!--Instagram-->
+                    <a class="ins-ic">
+                        <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+                    </a>
+
+                    <a class="mail-ic">
+                        <i class="far fa-envelope fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+                    </a>
+                </div>
+            </div>
+            <!-- Grid column -->
+        </div>
+        <!-- Grid row-->
+    </div>
+    <!-- Footer Elements -->
+
+</footer>
+<!-- Footer -->
+
+<!-- SCRIPTS -->
+<!-- JQuery -->
+<script type="text/javascript" src="JS/jquery-3.4.1.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="JS/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="JS/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="JS/mdb.min.js"></script>
+<script type="text/javascript" src="JS/main.js"></script>
+</body>
+
 </html>
