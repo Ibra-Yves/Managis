a542ab02947d5ea764b6b7f9d4830b64253920a4
RemyVase <44263729+RemyVase@users.noreply.github.com>
2019-10-02 23:28:03 +0200

    Formulaire pour ajouter un évent
    Il faut modifier pseudo de l'invité car pas bien aligné + modifier la textarea pour qu'elle prenne tout le bas c'est plus beau

---
+++ Web/MANAGIS/INC/addEvent.php
@@ -0,0 +1,42 @@
+<div class="container">
+    <div class="row centered-form">
+        <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Créez votre propre événement !</h3>
+                </div>
+                <div class="panel-body">
+                    <form id="formCreaEvent" class="formCreaEvent" name="formCreaEvent" method="post" action="validation.html">
+                        <div class="form-group">
+                            <input type="text" name="nom" id="nom" class="form-control input-sm" placeholder="Nom de l'événement">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="ville" id="ville" class="form-control input-sm" placeholder="Ville de l'hôte">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="rue" id="rue" class="form-control input-sm" placeholder="Rue de l'hôte">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="numero" id="numero" class="form-control input-sm" placeholder="Numéro de maison de l'hôte">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="date" id="date" class="form-control input-sm" placeholder="Date de l'événement">
+                        </div>
+                        <div class="form-group" >
+                            <div class="col-xs-9 col-sm-9 col-md-9">
+                                <input type="text" name="invite" id="invite" class="form-control input-sm" placeholder="Pseudo de l'invité">
+                            </div>
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary" type="button">Ajouter</button>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <textarea readonly class="listeInvite" id="listeInvite" rows="3" placeholder="Liste d'invités"></textarea>
+                        </div>
+                        <input type="submit" value="Ajouter l'événement" class="btn btn-primary align-middle">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
