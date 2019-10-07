83a6bb549d7cd98d691a2a3c8f2da364585c6034
RemyVase <44263729+RemyVase@users.noreply.github.com>
2019-10-07 17:11:52 +0200

    Create gestionCompte.php

---
+++ Web/MANAGIS/INC/gestionCompte.php
@@ -0,0 +1,18 @@
+<div class="container">
+  <div>
+    <h2>Gestion de compte</h2>
+    Pseudo : 
+    <br>
+    Mail : 
+    <br>
+    Date de création du compte : 
+    <br>
+  </div>
+  <div>
+    <h2>Voulez-vous changer de mot de passe ?</h2>
+    <input type="text" name="ancienMDP" id ="ancienMDP" placeholder="Ancien mot de passe">
+    <input type="text" name="newMDP" id="newMDP" placeholder="Nouveau mot de passe"> <input type="text" name="newMDPconfirm" id="newMDPconfirm" placeholder="Nouveau mot de passe" required>
+    <br>
+    <button type="button" id="changerMDP" class="btn btn-primary">Changer le mot de passe</button>
+  </div>
+</div>
