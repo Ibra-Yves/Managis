857f30b35c47646d85586693b699fe38b75bf22d
Ibrahima <44869843+Ibra-Yves@users.noreply.github.com>
2019-10-10 10:52:13 +0200

    Create App.js

---
+++ Application/Components/App.js
@@ -0,0 +1,43 @@
+import React, { Component} from 'react'
+import {
+	StyleSheet,
+	Text,
+	View,
+	StatusBar
+}
+from 'react-native';
+
+
+import Login from './Components/Login.js';
+import Signup from './Components/Signup.js';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
+export default class App extends Component {
+  render() {
+    return (
+     <View style={styles.container}>
+	<KeyboardAwareScrollView>
+	 <StatusBar
+	 backgroundColor="white"
+	 barStyle="light-content"
+	 />
+	 
+	 <Login/>
+	
+	 </KeyboardAwareScrollView>
+	 
+	 </View>
+
+	
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+	container : {
+		backgroundColor: 'black',
+		flexGrow: 1,
+		alignItems:'center',
+		justifyContent:'center'
+	}
+});
