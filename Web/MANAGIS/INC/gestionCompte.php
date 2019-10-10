be3146762221554683562c5a363bc912e07bb334
Liber Maxime <44001018+LiberTMx@users.noreply.github.com>
2019-10-10 11:51:38 +0200

    Update gestionCompte.php

--- Web/MANAGIS/INC/gestionCompte.php
+++ Web/MANAGIS/INC/gestionCompte.php
@@ -1,15 +1,60 @@
-<div class="container">
-  <div id="infoCompte">
-  </div>
-  <div>
-    <h2>Voulez-vous changer de mot de passe ?</h2>
-      <form id="formNewMdp" class="formNewMdp" name="formNewMdp" method="post" action="validation.php">
-          <div id="errorForm"></div>
-        <input type="password" name="ancienMDP" id ="ancienMDP" placeholder="Ancien mot de passe">
-        <input type="password" name="newMDP" id="newMDP" placeholder="Nouveau mot de passe">
-        <input type="password" name="newMDPconfirm" id="newMDPconfirm" placeholder="Nouveau mot de passe">
-        <br>
-        <input type="submit" id="changerMDP" value="Changez votre mot de passe" class="btn btn-primary">
-      </form>
-  </div>
+<div class="container-fluid">
+    <div class="row text-center">
+        <div class="col-md-6 ">
+            <div class="card mb-3 transparent" style="max-width: 110rem;">
+                <div class="card-header">Détails de votre compte</div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th style="width: 25%" scope="col" class="taillePolice">Pseudo</th>
+                                <th style="width: 25%" scope="col" class="taillePolice">Mail</th>
+                                <th style="width: 20%" scope="col" class="taillePolice">Date de création de compte</th>
+
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td class="taillePolice">Momo</td>
+                                <td class="taillePolice">2@qq</td>
+                                <td class="taillePolice">19/02/1996</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="infoCompte" class="row text-center">
+    <div class="col-md-6 ">
+        <div class="card mb-3 transparent" style="max-width: 110rem;">
+            <div class="card-header">Changement mot de passe</div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th style="width: 5%" scope="col" class="taillePolice">Ancien mot de passe</th>
+                            <th style="width: 25%" scope="col" class="taillePolice">Nouveau mot de passe</th>
+                            <th style="width: 25%" scope="col" class="taillePolice">Confirmation</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <!--<th scope="row">1</th>-->
+                            <td class="taillePolice"><input type="password" name="ancienMDP" id ="ancienMDP" placeholder="Ancien mot de passe"></td>
+                            <td class="taillePolice"><input type="password" name="newMDP" id="newMDP" placeholder="Nouveau mot de passe"></td>
+                            <td class="taillePolice"><input type="password" name="newMDPconfirm" id="newMDPconfirm" placeholder="Nouveau mot de passe"></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <button type="button" id="*" class="btn btn-primary boutonEvent">Confirmer</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
