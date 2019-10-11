733843ef36b154f3fb5b1a8f6c8b36fef36b6f1f
domad007 <44199654+domad007@users.noreply.github.com>
2019-10-11 12:42:48 +0200

    ALO

--- Web/MANAGIS/INC/pageEventInfos.php
+++ Web/MANAGIS/INC/pageEventInfos.php
@@ -1,5 +1,3 @@
-<div id="Maxime"></div>
-<div id="dom"></div>
 <div class="card transparent mb-3" style="max-width: 110rem;">
                 <div class="card-header">Listes des invités</div>
                 <div class="card-body">
