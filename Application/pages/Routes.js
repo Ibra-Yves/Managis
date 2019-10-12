2016a869c164bd7a76351895e84e116f6878f05f
Ibrahima <44869843+Ibra-Yves@users.noreply.github.com>
2019-10-12 11:41:19 +0200

    Update Routes.js

--- Application/pages/Routes.js
+++ Application/pages/Routes.js
@@ -1,14 +1,15 @@
 import React, { Component} from 'react'
 
-import { Router, Stack, Scene } from 'react-native-router-flux'
+import { Router, Stack, Scene } from 'react-native-router-flux';
 
 import Login from '../Components/Login.js';
 import Signup from '../Components/Signup.js';
 
-import { createAppContainer } from '@react-navigation/native';
-import { createStackNavigator} from 'react-navigation-stack';
+//import { createAppContainer } from '@react-navigation/native';
+//import { createStackNavigator} from 'react-navigation-stack';
 
-const AppContainer = createAppContainer(AppNavigator);
+//const AppContainer = createAppContainer(AppNavigator);
+
 
 export default class Routes extends Component {
     render(){
