6015b1987d5a0f4e1687ebca226b9943646c0970
domad007 <44199654+domad007@users.noreply.github.com>
2019-10-12 11:59:08 +0200

    connerie de max fix

--- Web/MANAGIS/INC/pageEvent.php
+++ Web/MANAGIS/INC/pageEvent.php
@@ -1,28 +1,26 @@
+<div class="container-fluid ">
     <div class="row text-center">
         <div class="col-md-6 ">
             <div class="card mb-3 transparent" style="max-width: 110rem;">
                 <div class="card-header">Détails de l'événement</div>
                 <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-striped">
-                        <thead>
-                        <tr>
-                            <th style="width: 5%" scope="col" class="taillePolice">#</th>
-                            <th style="width: 25%" scope="col" class="taillePolice">Nom de l'événement</th>
-                            <th style="width: 25%" scope="col" class="taillePolice">Nom de l'hôte</th>
-                            <th style="width: 20%" scope="col" class="taillePolice">Date de l'événement</th>
-                            <th style="width: 25%" scope="col" class="taillePolice">Adresse</th>
-                        </tr>
-                        </thead>
-                        <tbody id="infoSoiree">
-                        </tbody>
-                    </table>
-                </div>
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th style="width: 5%" scope="col" class="taillePolice">#</th>
+                                <th style="width: 25%" scope="col" class="taillePolice">Nom de l'événement</th>
+                                <th style="width: 25%" scope="col" class="taillePolice">Nom de l'hôte</th>
+                                <th style="width: 20%" scope="col" class="taillePolice">Date de l'événement</th>
+                                <th style="width: 25%" scope="col" class="taillePolice">Adresse</th>
+                            </tr>
+                            </thead>
+                            <tbody id="infoSoiree">
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
             <div id="fournitures"></div>
-    </div>
-    <div class="col-md-6" id="listInvites"></div>
-    <div id="fournitures"></div>
-</div>
+        </div>
         <div class="col-md-6" id="listeInvites"></div>
