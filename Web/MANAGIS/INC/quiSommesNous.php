39a132643199125b42d7362b53d119f78bb7ffcd
RemyVase <44263729+RemyVase@users.noreply.github.com>
2019-10-14 18:00:39 +0200

    Changement de style le style le style

--- Web/MANAGIS/INC/quiSommesNous.php
+++ Web/MANAGIS/INC/quiSommesNous.php
@@ -1,201 +1,167 @@
 <!-- Section: Team v.1 -->
-<section class="team-section text-center my-5">
+<section class="team-section text-center my-5" id="intro">
 
     <!-- Section heading -->
     <h2 class="h1-responsive font-weight-bold my-5">Qui sommes-nous?</h2>
-    <!-- Section description -->
-    <p class="grey-text w-responsive mx-auto mb-5">Voici l'équipe qui a travaillé afin de mener à bien ce projet</p>
+
 
     <!-- Grid row -->
-    <div class="row">
+    <div class="row" id="quiSommesNous">
 
         <!-- Grid column -->
         <div class="col-lg-3 col-md-6 mb-lg-0 mb-5">
-            <div class="avatar mx-auto">
-                <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg" class="rounded-circle z-depth-1"
-                     alt="Sample avatar">
+            <div class="card-wrapper ajustement-div">
+                <div id="card-3" class="card card-rotating text-center transparent">
+                    <!-- Front Side -->
+                    <div class="face front">
+                        <!-- Avatar -->
+                        <div class="avatar mx-auto transparent">
+                            <img src="IMG/ambroise.jpg" class="rounded-circle img-fluid" height="175px" width="175px"
+                                 alt="Third sample avatar image">
+                        </div>
+                        <!-- Content -->
+                        <div class="card-body ">
+                            <h4 class="font-weight-bold mt-1 mb-3">Ambroise MOSTIN</h4>
+                            <p class="font-weight-bold dark-grey-text">SCRUM MASTER</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <h5 class="font-weight-bold mt-4 mb-3">Ambroise MOSTIN</h5>
-            <p class="text-uppercase blue-text"><strong>SCRUM MASTER</strong></p>
-            <p class="grey-text">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
-                adipisci sed quia non numquam modi tempora eius.</p>
-            <ul class="list-unstyled mb-0">
-                <!-- Facebook -->
-                <a class="p-2 fa-lg fb-ic">
-                    <i class="fab fa-facebook-f blue-text"> </i>
-                </a>
-                <!-- Twitter -->
-                <a class="p-2 fa-lg tw-ic">
-                    <i class="fab fa-twitter blue-text"> </i>
-                </a>
-                <!-- Instagram -->
-                <a class="p-2 fa-lg ins-ic">
-                    <i class="fab fa-instagram blue-text"> </i>
-                </a>
-            </ul>
+        </div>
+
+        <!-- Grid column -->
+        <div class="col-lg-3 col-md-6 mb-lg-0 mb-5">
+            <div class="card-wrapper ajustement-div">
+                <div id="card-3" class="card card-rotating text-center transparent">
+                    <!-- Front Side -->
+                    <div class="face front">
+                        <!-- Avatar -->
+                        <div class="avatar mx-auto transparent">
+                            <img src="IMG/adri.jpg" class="rounded-circle img-fluid" height="175px" width="175px"
+                                 alt="Third sample avatar image">
+                        </div>
+                        <!-- Content -->
+                        <div class="card-body ">
+                            <h4 class="font-weight-bold mt-1 mb-3">Adrien CHELLE</h4>
+                            <p class="font-weight-bold dark-grey-text">PRODUCT OWNER</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
     <!-- Grid row -->
-    <div class="row">
+    <div class="row" id="quiSommesNous">
 
+        <!-- Grid column -->
         <div class="col-lg-3 col-md-6 mb-lg-0 mb-5">
-            <div class="avatar mx-auto">
-                <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(3).jpg" class="rounded-circle z-depth-1"
-                     alt="Sample avatar">
+            <div class="card-wrapper ajustement-div">
+                <div id="card-3" class="card card-rotating text-center transparent">
+                    <!-- Front Side -->
+                    <div class="face front">
+                        <!-- Avatar -->
+                        <div class="avatar mx-auto transparent">
+                            <img src="IMG/nico.png" class="rounded-circle img-fluid" height="175px" width="175px"
+                                 alt="Third sample avatar image">
+                        </div>
+                        <!-- Content -->
+                        <div class="card-body ">
+                            <h4 class="font-weight-bold mt-1 mb-3">Nicolas VIROUX</h4>
+                            <p class="font-weight-bold dark-grey-text">DEVELOPPEUR APPLICATION + RESEAU</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <h5 class="font-weight-bold mt-4 mb-3">Adrien CHELLE</h5>
-            <p class="text-uppercase blue-text"><strong>PRODUCT OWNER</strong></p>
-            <p class="grey-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem ipsa accusantium
-                doloremque rem laudantium totam aperiam.</p>
-            <ul class="list-unstyled mb-0">
-                <!-- Facebook -->
-                <a class="p-2 fa-lg fb-ic">
-                    <i class="fab fa-facebook-f blue-text"> </i>
-                </a>
-                <!-- Instagram -->
-                <a class="p-2 fa-lg ins-ic">
-                    <i class="fab fa-instagram blue-text"> </i>
-                </a>
-            </ul>
+        </div>
+
+        <!-- Grid column -->
+        <div class="col-lg-3 col-md-6 mb-lg-0 mb-5">
+            <div class="card-wrapper ajustement-div">
+                <div id="card-3" class="card card-rotating text-center transparent">
+                    <!-- Front Side -->
+                    <div class="face front">
+                        <!-- Avatar -->
+                        <div class="avatar mx-auto transparent">
+                            <img src="IMG/ibra.png" class="rounded-circle img-fluid" height="175px" width="175px"
+                                 alt="Third sample avatar image">
+                        </div>
+                        <!-- Content -->
+                        <div class="card-body ">
+                            <h4 class="font-weight-bold mt-1 mb-3">Ibrahima YVES</h4>
+                            <p class="font-weight-bold dark-grey-text">DEVELOPPEUR APPLICATION + RESEAU</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-        <!-- Grid column -->
 
     <!-- Grid row -->
-    <div class="row">
-        <div class="col-lg-3 col-md-6 mb-md-0 mb-5">
-            <div class="avatar mx-auto">
-                <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg" class="rounded-circle z-depth-1"
-                     alt="Sample avatar">
-            </div>
-            <h5 class="font-weight-bold mt-4 mb-3">Nicolas VIROUX</h5>
-            <p class="text-uppercase blue-text"><strong>Developpeur + Réseau</strong></p>
-            <p class="grey-text">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                mollit anim est fugiat nulla id eu laborum.</p>
-            <ul class="list-unstyled mb-0">
-                <!-- Facebook -->
-                <a class="p-2 fa-lg fb-ic">
-                    <i class="fab fa-facebook-f blue-text"> </i>
-                </a>
-                <!-- Instagram -->
-                <a class="p-2 fa-lg ins-ic">
-                    <i class="fab fa-instagram blue-text"> </i>
-                </a>
-                <!-- Dribbble -->
-                <a class="p-2 fa-lg ins-ic">
-                    <i class="fab fa-dribbble blue-text"> </i>
-                </a>
-            </ul>
-        </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <div class="col-lg-3 col-md-6">
-            <div class="avatar mx-auto">
-                <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(32).jpg" class="rounded-circle z-depth-1"
-                     alt="Sample avatar">
-            </div>
-            <h5 class="font-weight-bold mt-4 mb-3">Ibrahima YVES</h5>
-            <p class="text-uppercase blue-text"><strong>Developpeur + Réseau</strong></p>
-            <p class="grey-text">Perspiciatis repellendus ad odit consequuntur, eveniet earum nisi qui consectetur
-                totam officia voluptates perferendis voluptatibus aut.</p>
-            <ul class="list-unstyled mb-0">
-                <!-- Facebook -->
-                <a class="p-2 fa-lg fb-ic">
-                    <i class="fab fa-facebook-f blue-text"> </i>
-                </a>
-                <!-- Github -->
-                <a class="p-2 fa-lg ins-ic">
-                    <i class="fab fa-github blue-text"> </i>
-                </a>
-            </ul>
-        </div>
-        <!-- Grid column -->
-
-    </div>
-    <!-- Grid row -->
-
-    <div class="row">
+    <div class="row" id="quiSommesNous">
 
         <!-- Grid column -->
         <div class="col-lg-3 col-md-6 mb-lg-0 mb-5">
-            <div class="avatar mx-auto">
-                <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg" class="rounded-circle z-depth-1"
-                     alt="Sample avatar">
+            <div class="card-wrapper ajustement-div">
+                <div id="card-3" class="card card-rotating text-center transparent">
+                    <!-- Front Side -->
+                    <div class="face front">
+                        <!-- Avatar -->
+                        <div class="avatar mx-auto transparent">
+                            <img src="IMG/dominik.jpg" class="rounded-circle img-fluid" height="183px" width="183px"
+                                 alt="Third sample avatar image">
+                        </div>
+                        <!-- Content -->
+                        <div class="card-body">
+                            <h4 class="font-weight-bold mt-1 mb-3">Dominik FIEDORCZUK</h4>
+                            <p class="font-weight-bold dark-grey-text">DEVELOPPEUR WEB</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <h5 class="font-weight-bold mt-4 mb-3">Dominik FIEDORCZUK </h5>
-            <p class="text-uppercase blue-text"><strong>Developpeur web</strong></p>
-            <p class="grey-text">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
-                adipisci sed quia non numquam modi tempora eius.</p>
-            <ul class="list-unstyled mb-0">
-                <!-- Facebook -->
-                <a class="p-2 fa-lg fb-ic">
-                    <i class="fab fa-facebook-f blue-text"> </i>
-                </a>
-                <!-- Twitter -->
-                <a class="p-2 fa-lg tw-ic">
-                    <i class="fab fa-twitter blue-text"> </i>
-                </a>
-                <!-- Instagram -->
-                <a class="p-2 fa-lg ins-ic">
-                    <i class="fab fa-instagram blue-text"> </i>
-                </a>
-            </ul>
         </div>
-        <!-- Grid column -->
 
         <!-- Grid column -->
         <div class="col-lg-3 col-md-6 mb-lg-0 mb-5">
-            <div class="avatar mx-auto">
-                <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(3).jpg" class="rounded-circle z-depth-1"
-                     alt="Sample avatar">
+            <div class="card-wrapper ajustement-div">
+                <div id="card-3" class="card card-rotating text-center transparent">
+                    <!-- Front Side -->
+                    <div class="face front">
+                        <!-- Avatar -->
+                        <div class="avatar mx-auto transparent">
+                            <img src="IMG/Maxime.png" class="rounded-circle img-fluid" height="175px" width="175px"
+                                 alt="Third sample avatar image">
+                        </div>
+                        <!-- Content -->
+                        <div class="card-body">
+                            <h4 class="font-weight-bold mt-1 mb-3">Maxime LIBER</h4>
+                            <p class="font-weight-bold dark-grey-text">DEVELOPPEUR WEB</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <h5 class="font-weight-bold mt-4 mb-3">Maxime LIBER</h5>
-            <p class="text-uppercase blue-text"><strong>Developpeur web</strong></p>
-            <p class="grey-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem ipsa accusantium
-                doloremque rem laudantium totam aperiam.</p>
-            <ul class="list-unstyled mb-0">
-                <!-- Facebook -->
-                <a class="p-2 fa-lg fb-ic">
-                    <i class="fab fa-facebook-f blue-text"> </i>
-                </a>
-                <!-- Instagram -->
-                <a class="p-2 fa-lg ins-ic">
-                    <i class="fab fa-instagram blue-text"> </i>
-                </a>
-            </ul>
         </div>
-        <!-- Grid column -->
 
         <!-- Grid column -->
-        <div class="col-lg-3 col-md-6 mb-md-0 mb-5">
-            <div class="avatar mx-auto">
-                <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg" class="rounded-circle z-depth-1"
-                     alt="Sample avatar">
+        <div class="col-lg-3 col-md-6 mb-lg-0 mb-5">
+            <div class="card-wrapper ajustement-div">
+                <div id="card-3" class="card card-rotating text-center transparent">
+                    <!-- Front Side -->
+                    <div class="face front">
+                        <!-- Avatar -->
+                        <div class="avatar mx-auto transparent">
+                            <img src="IMG/remy.png" class="rounded-circle img-fluid" height="186px" width="186px"
+                                 alt="Third sample avatar image">
+                        </div>
+                        <!-- Content -->
+                        <div class="card-body">
+                            <h4 class="font-weight-bold mt-1 mb-3">Rémy VASE</h4>
+                            <p class="font-weight-bold dark-grey-text">DEVELOPPEUR WEB</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <h5 class="font-weight-bold mt-4 mb-3">Remy Vase</h5>
-            <p class="text-uppercase blue-text"><strong>Developpeur web</strong></p>
-            <p class="grey-text">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                mollit anim est fugiat nulla id eu laborum.</p>
-            <ul class="list-unstyled mb-0">
-                <!-- Facebook -->
-                <a class="p-2 fa-lg fb-ic">
-                    <i class="fab fa-facebook-f blue-text"> </i>
-                </a>
-                <!-- Instagram -->
-                <a class="p-2 fa-lg ins-ic">
-                    <i class="fab fa-instagram blue-text"> </i>
-                </a>
-                <!-- Dribbble -->
-                <a class="p-2 fa-lg ins-ic">
-                    <i class="fab fa-dribbble blue-text"> </i>
-                </a>
-            </ul>
         </div>
-        <!-- Grid column -->
-
     </div>
 
 </section>
-<!-- Section: Team v.1 -->
