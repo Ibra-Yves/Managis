f5cf561ab4d45773067e5fe3e7dcde85d4c121b5
RemyVase <44263729+RemyVase@users.noreply.github.com>
2019-10-15 18:10:58 +0200

    Un peu d'espace pour un site plus aéré
    Tcheu dis appelez moi Web Designer

--- Web/MANAGIS/INC/gestionCompte.php
+++ Web/MANAGIS/INC/gestionCompte.php
@@ -1,4 +1,4 @@
-<div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4 ajustement-div">
+<div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4 ajustement-div centered-form">
     <div class="panel-default transparent">
         <div class="panel-heading">
             <h3 class="panel-title taillePoliceSection" align="center">Détails du compte</h3>
