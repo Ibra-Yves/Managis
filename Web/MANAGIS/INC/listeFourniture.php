fb710c40edbc518828e550d2b21c3cc74daa0f34
RemyVase <44263729+RemyVase@users.noreply.github.com>
2019-10-15 18:00:33 +0200

    Ajout font-size + font-weight pour faire joli

--- Web/MANAGIS/INC/listeFourniture.php
+++ Web/MANAGIS/INC/listeFourniture.php
@@ -1,17 +1,17 @@
     <div class="row text-center">
         <div class="col-md-6 text-center" >
             <div class="card mb-3 transparent" style="max-width: 110rem;">
-                <div class="card-header blanc">Liste fournitures</div>
+                <div class="card-header blanc taillePoliceSection">Liste fournitures</div>
                 <div class="card-body">
                     <div class="table-responsive" style="max-height: 250px">
                     <form id="formQuantite" name="formQuantite" method="post" action="validation.html">
                         <table class="table table-striped">
                             <thead>
                             <tr>
-                                <th style="width: 10%" scope="col" class="taillePolice">#</th>
-                                <th style="width: 45%" scope="col" class="taillePolice">Fournitures</th>
-                                <th style="width: 15%"scope="col" class="taillePolice">Quantité</th>
-                                <th style="width: 15%"scope="col" class="taillePolice">Supprimer Fourniture</th>
+                                <th style="width: 10%" scope="col" class="taillePoliceTitre">#</th>
+                                <th style="width: 45%" scope="col" class="taillePoliceTitre">Fournitures</th>
+                                <th style="width: 15%"scope="col" class="taillePoliceTitre">Quantité</th>
+                                <th style="width: 15%"scope="col" class="taillePoliceTitre">Supprimer Fourniture</th>
                             </tr>
                             </thead>
                             <tbody id="listeFournitures" align="left">
@@ -25,9 +25,9 @@
         </div>
         <div class="col-md-6  text-center" >
             <div class="card mb-3 transparent" style="max-width: 110rem;">
-                <div class="card-header blanc">Fournitures nécessaires : </div>
+                <div class="card-header blanc taillePoliceSection">Fournitures nécessaires : </div>
                 <div class="card-body">
-                    <h5 class="card-title">Ajouter des fournitures !</h5>
+                    <h5 class="card-title font-weight-bold">Ajouter des fournitures !</h5>
                     <br>
                     <form id="formFournitures" name="formFournitures" method="post" action="validation.html">
                         <div id="errorForm"></div>
