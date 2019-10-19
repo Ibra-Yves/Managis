b5a45cb3581ffba194060369112bcf1042df6a09
Liber Maxime <44001018+LiberTMx@users.noreply.github.com>
2019-10-19 18:14:46 +0200

    les bails

---
+++ Web/INC/infoSup.php
@@ -0,0 +1,20 @@
+<div class="col-md-6 text-center">
+    <div class="card mb-3 transparent " style="max-width: 110rem;">
+        <div class="card-header blanc taillePoliceSection">Infos supplémentaires sur l'événement</div>
+        <div class="card-body">
+            <div class="table-responsive" style="max-height: 100px">
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th style="width: 5%" scope="col" class="taillePoliceTitre">Invités</th>
+                        <th style="width: 95%" scope="col" class="taillePoliceTitre">Fournitures</th>
+                        <th style="width: 95%" scope="col" class="taillePoliceTitre">Commentaires</th>
+                    </tr>
+                    </thead>
+                    <tbody id="infoSupp" align="left">
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
