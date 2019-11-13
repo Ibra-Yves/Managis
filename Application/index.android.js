ed4089fe24f9c6408a62f9547d3ad2818f8041f5
VirouxNicolas <33083942+VirouxNicolas@users.noreply.github.com>
2019-11-13 21:21:56 +0100

    Create index.android.js

---
+++ Application/index.android.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { AppRegistry,
+         View,
+        Text,
+        StyleSheet } from 'react-native';
+
+import UsersManager from './pages/app';
+AppRegistry.registerComponent('Managis', () => Managis);
