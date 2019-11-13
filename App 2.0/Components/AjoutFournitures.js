4a132aa7780265327adb1439ab4004a4e9b7f918
Ambroise Mostin <40889985+amostin@users.noreply.github.com>
2019-11-13 21:30:19 +0100

    Formulaires ajout event, invité, fourniture et commentaire

---
+++ App 2.0/Components/AjoutFournitures.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react'
+
+import {Text, View, StyleSheet, Image, TextInput, TouchableOpacity, ScrollView} from 'react-native'
+
+
+export default class AjoutFournitures extends Component {
+  render() {
+    return (
+      <ScrollView>
+      <View style={styles.containerTitre}>
+        <Text style={styles.titrePage}>Ajoute des invités !</Text>
+      </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style = {styles.inputBox}
+            placeholder = 'Nom founiture'
+            placeholderTextColor = '#FFFFFF'
+          />
+        </View>
+        <View style={styles.submitContainer}>
+          <TouchableOpacity>
+            <Text style={styles.submitButton}>Valider</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 350,
+    height: 350
+  },
+  logoContainer: {
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  },
+  inputContainer: {
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  },
+  submitContainer: {
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  },
+  inputBox : {
+		width:300,
+		backgroundColor:'#3A4750',
+		borderRadius: 25,
+		paddingVertical:12,
+		fontSize:16,
+		color:'#FFFFFF',
+		textAlign:'center',
+		marginVertical: 10
+	},
+  submitButton: {
+    backgroundColor:'#3A4750',
+		width:100,
+		borderRadius: 25,
+		marginVertical: 10,
+		paddingVertical: 13,
+    textAlign: 'center',
+    color: '#FFFFFF'
+  },
+  titrePage: {
+      color: '#FFFFFF',
+      fontSize: 18
+    },
+  containerTitre: {
+    backgroundColor:'#3A4750',
+    width:200,
+    borderRadius: 25,
+    marginVertical: 10,
+    paddingVertical: 13,
+    textAlign: 'center',
+  },
+  signupButton: {
+    textAlign: 'center',
+    marginVertical: 10,
+    paddingVertical: 13,
+    color: '#3A4750'
+
+  }
+})
