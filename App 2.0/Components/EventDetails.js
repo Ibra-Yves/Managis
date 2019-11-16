408ed1dcb9a12876cc7197939d632501102d080a
ATHOOS <43479148+ATHOOS@users.noreply.github.com>
2019-11-16 16:03:34 +0100

    ajout des details d'un event (ne fonctionne pas)

---
+++ App 2.0/Components/EventDetails.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import { Text, TouchableOpacity, StyleSheet } from 'react-native'
+
+
+
+ class EventDetails extends Component {
+  render() {
+    const {item} = this.props
+    return (
+      <TouchableOpacity>
+        <Text>ok</Text>
+      </TouchableOpacity>
+    )
+  }
+}
+
+export default EventDetails
