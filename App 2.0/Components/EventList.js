1572ecf18f226e94e8f1b11c64f133978e4412fa
ATHOOS <43479148+ATHOOS@users.noreply.github.com>
2019-11-16 15:59:17 +0100

    ajout d'une liste

--- App 2.0/Components/EventList.js
+++ App 2.0/Components/EventList.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react'
 
-import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity, Image, StyleSheet, FlatList } from 'react-native'
+import EventDetails from '../Components/EventDetails'
+
+/*je declare ici une serie d'event afin de tester la liste*/
+const EVENT = [
+  {
+    id: '1',
+    title: 'Soiree Monopoly',
+    date: 'date de l\'event',
+    heure: 'heure de l\'event'
+  },
+  {
+    id: '2',
+    title: 'Soiree react',
+    date: 'date de l\'event',
+    heure: 'heure de l\'event'
+  },
+  {
+    id: '3',
+    title: 'Soiree php',
+    date: 'date de l\'event',
+    heure: 'heure de l\'event'
+  }
+]
+
 
 class EventList extends Component {
   render() {
     return (
+
       <View>
-        <TouchableOpacity
-          onPress={() => this.props.navigation.openDrawer('EventDrawerNav')}>
-          <Image
-            source={require('../Images/icons8-menu-arrondi-50.png')}
-            style={styles.icon}
+      <TouchableOpacity
+        onPress={() => this.props.navigation.openDrawer('EventDrawerNav')}
+        style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Image
+          source={require('../Images/icons8-menu-arrondi-50.png')}
+          style={styles.icon}
           />
-        </TouchableOpacity>
+      </TouchableOpacity>
+        <FlatList
+          data={EVENT}
+          extraData={this.state}
+          renderItem={({item}) => (
+            <TouchableOpacity><Text>{item.title} {'\n'} {item.heure}</Text></TouchableOpacity>
+
+          )}
+        />
       </View>
     )
   }
@@ -22,6 +56,12 @@ const styles= StyleSheet.create({
   icon: {
     width: 30,
     height: 30
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   }
 })
 
