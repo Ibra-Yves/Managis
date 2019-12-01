3f53f4ec6a7699526578e9b2438e33905a50a6c9
VirouxNicolas <33083942+VirouxNicolas@users.noreply.github.com>
2019-12-01 23:47:10 +0100

    Add files via upload
    dans la bonne branche !!!!!!!!!

--- Application/DBRestes/createAnnonce.php
+++ Application/DBRestes/createAnnonce.php
@@ -3,13 +3,13 @@ include 'dbconfig.php';
 $con = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
 $json = file_get_contents('php://input');
 $obj = json_decode($json,true);
-$email = $obj['email'];
+$idUser = $obj['idUser'];
 $nomReste = $obj['nomReste'];
 $quantiteReste = $obj['quantiteReste'];
 $descriptionReste = $obj['descriptionReste'];
 $adresse = $obj['adresse'];
 
-$CheckSQL = "SELECT * FROM restes WHERE email='$email'";
+$CheckSQL = "SELECT * FROM gestionrestes WHERE idUser='$idUser'";
 $check = mysqli_fetch_array(mysqli_query($con,$CheckSQL));
 if(isset($check)){
  $RestExistMSG = 'Cette annonce a déjà été crée !';
@@ -17,8 +17,8 @@ $RestExistJson = json_encode($RestExistMSG);
  echo $RestExistJson ;
  }
  else{
-$Sql_Query = "insert into restes (email,nomReste,quantiteReste,descriptionReste,adresse)
-values ('$email','$nomReste','$quantiteReste','$descriptionReste','$adresse')";
+$Sql_Query = "insert into gestionrestes (idUser,nomReste,quantiteReste,descriptionReste,adresse)
+values ('$idUser','$nomReste','$quantiteReste','$descriptionReste','$adresse')";
  if(mysqli_query($con,$Sql_Query)){
 $MSG = 'Annonce enregistrée avec succès' ;
 $json = json_encode($MSG);
