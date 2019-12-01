ebb2afc15f932306c288fdd6c846b3c61224725f
VirouxNicolas <33083942+VirouxNicolas@users.noreply.github.com>
2019-12-01 23:52:31 +0100

    Update restes.js
    enfin l'affichage des restes bordel

--- Application/components/restes.js
+++ Application/components/restes.js
@@ -1,15 +1,65 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import { View,
-        Text,
-        TouchableOpacity,
-        Image, StyleSheet,
-        ScrollView,
-        TextInput } from 'react-native'
+import { AppRegistry, Image, ScrollView, TextInput, TouchableOpacity, StyleSheet, ActivityIndicator, Text, View, Alert } from 'react-native';
+import ListView from "deprecated-react-native-listview";
 
 class Restes extends Component {
-  render() {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    }
+  }
+GetItem (nomReste) {
+
+  Alert.alert(nomReste);
+
+  }
+
+
+  componentDidMount() {
+
+    return fetch('http://192.168.0.9/ManagisApp/DBRestes/restes.php')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.setState({
+          isLoading: false,
+          dataSource: ds.cloneWithRows(responseJson),
+        }, function() {
+          // In this block you can do something with new state.
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  ListViewItemSeparator = () => {
     return (
+      <View
+        style={{
+          height: .5,
+          width: "100%",
+          backgroundColor: "#000",
+        }}
+      />
+    );
+  }
+
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
+    return (
+
       <ScrollView>
         <View style={styles.containerTitre}>
 		<TouchableOpacity
@@ -24,17 +74,17 @@ class Restes extends Component {
           <View style={{flex: 6, justifyContent: 'center'}}>
             <Text style={styles.titrePage}>Marché des restes</Text>
           </View>
-          <View style={{flex : 1}}>
-		  <TouchableOpacity
-            onPress={() => this.props.navigation.openDrawer('myNav')}
-            style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Image
-              source={require('../image/icons8-menu-arrondi-50.png')}
-              style={styles.icon}
-            />
-          </TouchableOpacity>
           </View>
-        </View>
+          <View style={{flex : 1}}>
+          <TouchableOpacity
+                onPress={() => this.props.navigation.openDrawer('myNav')}
+                style={{flex: 1, marginTop: 10, justifyContent: 'center',alignItems:'center'}}>
+                <Image
+                  source={require('../image/icons8-menu-arrondi-50.png')}
+                  style={styles.icon}
+                  />
+              </TouchableOpacity>
+          </View>
         <View style={{margin: 5, alignItems: 'center'}}>
           <TextInput
             style={styles.textinput}
@@ -49,43 +99,35 @@ class Restes extends Component {
               <Text style={styles.searchText}>Rechercher</Text>
             </TouchableOpacity>
           </View>
-        </View>
+      <View style={styles.MainContainer}>
+
+        <ListView
+
+          dataSource={this.state.dataSource}
+
+          renderSeparator= {this.ListViewItemSeparator}
+
+          renderRow={(rowData) => <Text style={styles.rowViewContainer}
+          onPress={this.GetItem.bind(this, rowData.nomReste , rowData.adresse)} >{rowData.nomReste}</Text>}
+        />
+      </View>
+      </View>
       </ScrollView>
-    )
+    );
   }
 }
 
-const styles= StyleSheet.create({
-  icon: {
-    width: 30,
-    height: 30
-  },
-  iconSearch: {
-    width: 20,
-    height: 20,
-    margin: 2
-  },
-  searchText: {
-    alignItems: 'center',
-    fontSize: 16,
-    color: '#3A4750'
-  },
-  textinput: {
-    width:300,
-		backgroundColor:'#3A4750',
-		borderRadius: 25,
-		paddingVertical:12,
-		fontSize:16,
-		color:'#FFFFFF',
-		textAlign:'center',
-		marginVertical: 10
-  },
-  containerTitre: {
-    backgroundColor:'#3A4750',
-    flexDirection: 'row',
-    height: 60
-  },
-  titrePage: {
+const styles = StyleSheet.create({
+
+MainContainer :{
+
+justifyContent: 'center',
+flex:1,
+margin: 10
+
+},
+
+titrePage: {
     color: '#FFFFFF',
     fontSize: 18,
     textAlign: 'center'
@@ -94,7 +136,49 @@ const styles= StyleSheet.create({
     alignItems: 'center',
     fontSize: 16,
 
-  }
-})
+  },
+
+  icon: {
+    width: 30,
+    height: 30
+  },
+
+  iconSearch: {
+    width: 20,
+    height: 20,
+    margin: 2
+  },
+
+  textinput: {
+   width:300,
+   backgroundColor:'#3A4750',
+   borderRadius: 25,
+   paddingVertical:12,
+   fontSize:16,
+   color:'#FFFFFF',
+   textAlign:'center',
+   marginVertical: 10
+ },
+
+containerTitre: {
+    backgroundColor:'#3A4750',
+    flexDirection: 'row',
+    height: 60
+  },
+
+   rowViewContainer: {
+        fontSize: 20,
+        paddingRight: 10,
+        paddingTop: 10,
+        paddingBottom: 10,
+      },
+
+  searchText: {
+   alignItems: 'center',
+   fontSize: 16,
+   color: '#3A4750'
+ },
+
+});
 
 export default Restes
