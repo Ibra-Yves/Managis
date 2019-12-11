d853b74acc99a5ad7212a33345f9a42019545677
domad007 <44199654+domad007@users.noreply.github.com>
2019-12-11 13:00:34 +0100

    Creation de la redirection + config adequate

--- Web/INC/infoSupAno.php
+++ Web/INC/infoSupAno.php
@@ -111,10 +111,10 @@ function listeParticipant($idEvent)
     <link href="CSS/agency.css" rel="stylesheet">
 
     <!-- JQuery -->
-    <script type="text/javascript" src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script type="text/javascript" src="vendor/jquery/jquery.js"></script>
+    <link rel="stylesheet" href="vendor/jquery/jquery-ui.css">
+    <script src="vendor/jquery/jquery-1.12.4.js"></script>
+    <script src="vendor/jquery/jquery-ui.js"></script>
     <!-- Bootstrap tooltips -->
     <script type="text/javascript" src="JS/popper.min.js"></script>
     <!-- MDB core JavaScript -->
